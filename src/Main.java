0a2a0ed4bb3f3de4f9406dd952bf570fd9c65227
Gamma212 <157800823+Gamma212@users.noreply.github.com>
2024-04-12 23:22:23 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,25 @@
+public class Main {
+
+
+
+    public static void main(String[] args) {
+
+
+
+        int ticket_price = 13_676;
+
+
+
+
+
+
+
+        System.out.println(ticket_price/20);
+
+
+
+
+
+    }
+
+}
